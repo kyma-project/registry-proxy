fd9f611e4a5e2c96e5e86f0ab2c956a7d6481b41
Piotr Halama <piotr.halama@sap.com>
2025-01-20 09:12:31 +0100

    fix: missing required spec field in test (#30)

--- internal/controller/imagepullreverseproxy_controller_test.go
+++ internal/controller/imagepullreverseproxy_controller_test.go
@@ -35,7 +35,9 @@ var _ = Describe("ImagePullReverseProxy Controller", func() {
 						Name:      resourceName,
 						Namespace: "default",
 					},
-					// TODO(user): Specify other spec details if needed.
+					Spec: operatorkymaprojectiov1alpha1.ImagePullReverseProxySpec{
+						TargetHost: "dummy",
+					},
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}

91c21a8b7896f8be7c5415e025cc4efb1bf53fe6
Piotr Halama <piotr.halama@sap.com>
2025-05-27 09:02:27 +0200

    fix busola RBAC (#182)

--- components/operator/rbac.go
+++ components/operator/rbac.go
@@ -4,6 +4,7 @@ package operator
 // +kubebuilder:rbac:groups=operator.kyma-project.io,resources=registryproxies/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=operator.kyma-project.io,resources=registryproxies/finalizers,verbs=update
 
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=list;get;watch;create;update;patch;delete
 
 // +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=list;get;watch;create;update;patch;delete

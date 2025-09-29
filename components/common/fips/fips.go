f3a484ca058b56d5fbb516b090d9359eda5fa82c
Piotr Halama <piotr.halama@sap.com>
2025-09-29 19:42:10 +0200

    comment

--- components/common/fips/fips.go
+++ components/common/fips/fips.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// IsFIPS140Only checks if the application is running in FIPS 140 exclusive mode.
 func IsFIPS140Only() bool {
 	return fips140.Enabled() && os.Getenv("GODEBUG") == "fips140=only,tlsmlkem=0"
 }
